7ad62a2643407ab458f27a8d8a64d563f0ae6130
Andrew Ealovega <Andrew@Ealovega.dev>
2023-11-28 17:55:37 -0500

    Fix js alerts

--- papersplease/orm/connect.py
+++ papersplease/orm/connect.py
@@ -7,7 +7,7 @@ from .model import Account, Conference, Assignment, Paper, PaperAuthor
 sqlite_file_name = "db.sqlite"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(sqlite_url, echo=True, connect_args={"check_same_thread": False})
 
 
 def prepare_db():
